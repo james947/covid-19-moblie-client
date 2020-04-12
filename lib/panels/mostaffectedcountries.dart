0e9fcd3a21c5b22482324d7e1e8e3d354777e83b
james947 <jmuriuki947@gmail.com>
2020-04-12 23:12:40 +0300

    Add most affected countries

---
+++ lib/panels/mostaffectedcountries.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MostAffectedPanel extends StatelessWidget {
+
+  final List countryData;
+
+  const MostAffectedPanel({Key key, this.countryData}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemBuilder: (context, index){
+        return Container(
+          margin: EdgeInsets.symmetric(horizontal:10.0, vertical: 10.0),
+          child: Row(
+            children: <Widget>[
+              Image.network(countryData[index]['countryInfo']['flag'], height:25.0,),
+              SizedBox(width: 10.0,),
+              Text(countryData[index]['country'], style: TextStyle(fontWeight: FontWeight.bold),),
+              SizedBox(width: 10.0,),
+              Text('Deahts:' + countryData[index]['deaths'].toString(), style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold))
+            ],
+          ),
+        );
+      },
+      itemCount: 5,),
+    );
+  }
+}
