f4e18b2f97ad102d46e848ded8eeae05cc64575e
james947 <jmuriuki947@gmail.com>
2020-04-12 23:12:56 +0300

    Add world wide panel

--- lib/panels/worldwidepanel.dart
+++ lib/panels/worldwidepanel.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class WorldWidePanel extends StatelessWidget {
+
+  final Map worldData;
+
+  const WorldWidePanel({Key key, this.worldData}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,10 +14,30 @@ class WorldWidePanel extends StatelessWidget {
         physics: NeverScrollableScrollPhysics(),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 2),
         children: <Widget>[
-          StatusPanel(),
-          StatusPanel(),
-          StatusPanel(),
-          StatusPanel(),
+          StatusPanel(
+            title: 'Confirmed',
+            panelColor: Colors.grey[400],
+            textColor: Colors.grey[900],
+            count: worldData['cases'].toString(),
+          ),
+          StatusPanel(
+            title: 'Active',
+            panelColor: Colors.blue[100],
+            textColor: Colors.blue[900],
+            count: worldData['active'].toString(),
+          ),
+          StatusPanel(
+            title: 'Recovered',
+            panelColor: Colors.green[100],
+            textColor: Colors.green[900],
+            count: worldData['recovered'].toString(),
+          ),
+          StatusPanel(
+            title: 'Deaths',
+            panelColor: Colors.red[100],
+            textColor: Colors.red[900],
+            count: worldData['deaths'].toString(),
+          ),
         ],
       ),
     );
@@ -20,28 +45,38 @@ class WorldWidePanel extends StatelessWidget {
 }
 
 class StatusPanel extends StatelessWidget {
+
+  final Color panelColor;
+  final Color textColor;
+  final String title;
+  final String count;
+
+  const StatusPanel({Key key, this.panelColor, this.textColor, this.title, this.count}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return Container(
       margin: EdgeInsets.all(5.0),
       height: 80, width: width/2,
-      color: Colors.blue[100],
+      color: panelColor ,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center  ,
         children: <Widget>[
           Text(
-            'Confirmed',
-            style: TextStyle(
+              title,
+              style: TextStyle(
               fontWeight: FontWeight.bold,
-              fontSize: 16.0
+              fontSize: 16.0,
+              color: textColor,
             ),
           ),
           Text(
-            '1234',
+            count,
             style: TextStyle(
               fontSize: 16.0,
-              fontWeight: FontWeight.bold
+              fontWeight: FontWeight.bold,
+              color: textColor,
             ),
           )
         ],
